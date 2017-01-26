8e3b898138ad3d8b2a45faae47f6528f3e004303
Warner Losh <imp@freebsd.org>
2017-01-26 17:28:59 +0100

    api: bootelf: go: flush cache before starting
    Some application might load some code at location that contain stale
    cache entries. Before running a elf or raw binary, flush the caches
    if they are enabled.
    
    Reviewed-by: Tom Rini <trini@konsulko.com>
    Signed-off-by: Emmanuel Vadot <manu@bidouilliste.com>

--- api/api.c
+++ api/api.c
@@ -307,6 +307,11 @@ static int API_dev_close(va_list ap)
 	if (!err)
 		di->state = DEV_STA_CLOSED;
 
+	if (dcache_status())
+		flush_dcache_all();
+	if (icache_status())
+		invalidate_icache_all();
+
 	return err;
 }
 
